6ae2e10c3cc5bba16b0294d777a8ac1a25d15e7d
inmount <inmount@163.com>
2019-07-19 10:12:21 +0800

    1.0.1907.2

--- dotnet-core-simple-session-client/Demo/Program.cs
+++ dotnet-core-simple-session-client/Demo/Program.cs
@@ -3,17 +3,30 @@
 namespace Demo {
     class Program {
         static void Main(string[] args) {
-            using (SimpleSessionClient.Session session = new SimpleSessionClient.Session("127.0.0.1", 8601, "000000")) {
 
-                // 创建一个新的交互标识
-                if (!session.SetSessionID("c83e7a19bf2b4c9f861465fd243696d2")) session.CreateNewSessionID();
-                Console.WriteLine("ID " + session.SessionID);
+            //string ip = "127.0.0.1";
+            //string ip = "39.106.190.110";
+            //string ip = "111.73.45.233";
+            string ip = "111.73.46.139";
 
-                Console.WriteLine("Test before " + session["Test"]);
-                session["Test"] = Guid.NewGuid().ToString();
-                Console.WriteLine("Test after " + session["Test"]);
+            //连接100次，测试反应速度
+            for (int i = 0; i < 100; i++) {
+                int tick = Environment.TickCount;
+                using (SimpleSessionClient.Session session = new SimpleSessionClient.Session(ip, 8601, "000000")) {
 
+                    // 创建一个新的交互标识
+                    //if (!session.SetSessionID("c83e7a19bf2b4c9f861465fd243696d2")) session.CreateNewSessionID();
+                    session.CreateNewSessionID();
+                    Console.WriteLine("ID " + session.SessionID);
+
+                    //Console.WriteLine("Test before " + session["Test"]);
+                    session["Test"] = Guid.NewGuid().ToString();
+                    Console.WriteLine("Test after " + session["Test"]);
+
+                }
+                Console.WriteLine("Take " + (Environment.TickCount - tick) + " ms");
             }
+
         }
     }
 }
